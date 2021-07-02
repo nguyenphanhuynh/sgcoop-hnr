1ab9b076b8ec264aaabd7bbea073affb1a9d2934
Huynh NGUYEN <huynh.nguyen@snapmart.ph>
2021-07-02 16:16:53 +0700

    update log

--- auth.js
+++ auth.js
@@ -44,7 +44,7 @@ router.get("/callback", (req, res, next) => {
 
 router.get("/logout", (req, res) => {
     req.logOut();
-    console.log('req: ', req);
+    console.log('req.protocolreq: ', req.protocol);
     let returnTo = req.protocol + "://" + req.hostname;
     const port = req.connection.localPort;
 
